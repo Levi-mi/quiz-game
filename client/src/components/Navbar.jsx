d1a91d5d224375ebe339b1f728b0071edd265268
Netanel <128292678+NetanelSegal@users.noreply.github.com>
2025-01-02 14:10:47 +0200

    user shit (#1)

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,35 +1,31 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../useManager/authContext';
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
+  return (
+    <div className="navbar-container">
+      <Link to={'/'}>Logo(welcome User?)</Link>
+      <div>
+        {user ? (
+          <>
+            <Link to={'/profile'}>Profile</Link>
+            <button onClick={logout}>Logout</button>
+          </>
+        ) : (
+          <>
+            <Link to={'/login'}>
+              <button>Login</button>
+            </Link>
+            <Link to={'/register'}>
+              <button>Register</button>
+            </Link>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    const navigate = useNavigate();
-
-    const handleHome = () => {
-        navigate('/');
-    }
-
-    const handleLogin = () => {
-        navigate('/login');
-    }
-
-    const handleRegister = () => {
-        navigate('/register');
-    }
-
-    const handleLogout = () => {
-        navigate('/');
-    }
-
-    return (
-        <div className='navbar-container'>
-            <div>Logo(welcome User?)</div>
-            <div>
-                <button onClick={handleHome}>Home</button>
-                <button onClick={handleLogout}>Logout</button>
-            </div>
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- server/src/db/connectToDB.js
+++ server/src/db/connectToDB.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose"
 const connectToDB = async () => {
   try {
     console.log("db is connecting...")
-    await mongoose.connect("mongodb+srv://User:aA123456@cluster0.4cfey.mongodb.net/Quiz?retryWrites=true&w=majority&appName=Cluster0/Quiz"
-)
+    await mongoose.connect("mongodb://localhost:27017/levi")
     console.log("db is connected")
   } catch (error) {
     console.log(error)
